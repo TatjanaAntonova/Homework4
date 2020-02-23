dfc17c96547999c41f425c634c5cfcf518fbd0a9
Tatjana Antonova <taanto@ttu.ee>
2020-02-23 15:58:01 +0200

    Add Measure and Entity to Domain folder

---
+++ Domain/Entity.cs
@@ -0,0 +1,14 @@
+﻿using Homework4.Data.Common;
+
+namespace Homework4.Domain
+{
+    public abstract class Entity<T> where T: PeriodData
+    {
+        public T Data { get; }
+
+        protected Entity(T data)
+        {
+            Data = data;
+        }
+    }
+}
---
+++ Domain/Quantity/Measure.cs
@@ -0,0 +1,11 @@
+﻿using Homework4.Data.Quantity;
+
+namespace Homework4.Domain.Quantity
+{
+    public class Measure: Entity<MeasureData>
+    {
+        public Measure(MeasureData data) : base(data)
+        {
+        }
+    }
+}
