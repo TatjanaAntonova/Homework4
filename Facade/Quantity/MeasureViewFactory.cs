bcb6adb782863ed2a84b5872b6791f1cc126d15f
Tatjana Antonova <taanto@ttu.ee>
2020-02-23 17:45:39 +0200

    Add MeasureViewFactory

---
+++ Facade/Quantity/MeasureViewFactory.cs
@@ -0,0 +1,36 @@
+﻿using Homework4.Data.Quantity;
+using Homework4.Domain.Quantity;
+
+namespace Homework4.Facade.Quantity
+{
+    public static class MeasureViewFactory
+    {
+        public static Measure Create(MeasureView v)
+        {
+            var d = new MeasureData
+            {
+                Id = v.Id,
+                Name = v.Name,
+                Code = v.Code,
+                Definition = v.Definition,
+                ValidFrom = v.ValidFrom,
+                ValidTo = v.ValidTo
+            };
+            return new Measure(d);
+        }
+
+        public static MeasureView Create(Measure o)
+        {
+            var v = new MeasureView
+            {
+                Id = o.Data.Id,
+                Name = o.Data.Name,
+                Code = o.Data.Code,
+                Definition = o.Data.Definition,
+                ValidFrom = o.Data.ValidFrom,
+                ValidTo = o.Data.ValidTo
+            };
+            return v;
+        }
+    }
+}
