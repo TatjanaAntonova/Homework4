69a9c928e2a7c3629c6ee8614014f705c705e6f2
Tatjana Antonova <taanto@ttu.ee>
2020-03-17 18:04:40 +0200

    31. osa - Testimise abifunktsioonide lisamine

--- Tests/BaseTest.cs
+++ Tests/BaseTest.cs
@@ -1,11 +1,17 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Homework4.Aids;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 
 namespace Homework4.Tests
 {
     public abstract class BaseTest<TClass, TBaseClass>
     {
+        private const string notTested = "<{0}> is not tested";
+        private const string notSpecified = "Class is not specified";
+        private List<string> members { get; set; }
         protected TClass obj;
         protected Type type;
 
@@ -20,6 +26,33 @@ namespace Homework4.Tests
         {
             Assert.AreEqual(typeof(TBaseClass), type.BaseType);
         }
+
+        [TestMethod]
+        public void IsTested()
+        {
+            if (type == null) Assert.Inconclusive(notSpecified);
+            var m = GetClass.Members(type, PublicBindingFlagsFor.DeclaredMembers);
+            members = m.Select(e => e.Name).ToList();
+            removeTested();
+
+            if (members.Count == 0) return;
+            Assert.Fail(notTested, members[0]);
+        }
+
+        private void removeTested()
+        {
+            var tests = GetType().GetMembers().Select(e => e.Name).ToList();
+
+            for (var i = members.Count; i > 0; i--)
+            {
+                var m = members[i - 1] + "Test";
+                var isTested = tests.Find(o => o == m);
+
+                if (string.IsNullOrEmpty(isTested)) continue;
+                members.RemoveAt(i - 1);
+            }
+        }
+
         protected static void isNullableProperty<T>(Func<T> get, Action<T> set) 
         {
            isProperty(get, set);
---
+++ Tests/Data/IsDataTested.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Homework4.Tests.Data
+{
+    [TestClass]
+    public class IsDataTested: AssemblyTests
+    {
+        private const string assembly = "Homework4.Data";
+
+        protected override string Namespace(string name) { return $"{assembly}.{name}"; }
+        [TestMethod]
+        public void IsCommonTested() { isAllTested(assembly, Namespace("Common"));}
+
+        [TestMethod]
+        public void IsMoneyTested() { isAllTested(assembly, Namespace("Money")); }
+
+        [TestMethod]
+        public void IsQuantityTested() { isAllTested(assembly, Namespace("Quantity")); }
+        
+        [TestMethod]
+        public void IsTested() { isAllTested(assembly,base.Namespace("Data")); }
+    }
+}
