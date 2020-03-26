24e1b2f0bd50ae5f8d4fc4ac122840bf486998ce
Tatjana Antonova <taanto@ttu.ee>
2020-03-26 17:00:21 +0200

    HtmlExtension meetodite testide raamistikud -started

---
+++ Tests/Pages/Extensions/DetailsTableForHtmlExtensionTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Homework4.Tests.Pages.Extensions
+{
+    [TestClass]
+    public class DetailsTableForHtmlExtensionTests
+    {
+    }
+}
