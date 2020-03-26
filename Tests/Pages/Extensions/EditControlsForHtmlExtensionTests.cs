35fd170b9c3b467c9540e9b21dc1009b4e2aa606
Tatjana Antonova <taanto@ttu.ee>
2020-03-26 21:54:31 +0200

    40. osa - HtmlExtension meetodite testimine Mockide abil

--- Tests/Pages/Extensions/EditControlsForHtmlExtensionTests.cs
+++ Tests/Pages/Extensions/EditControlsForHtmlExtensionTests.cs
@@ -1,4 +1,7 @@
-﻿using Homework4.Pages.Extensions;
+﻿using System.Collections.Generic;
+using Homework4.Facade.Quantity;
+using Homework4.Pages.Extensions;
+using Microsoft.AspNetCore.Html;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Homework4.Tests.Pages.Extensions
@@ -11,7 +14,16 @@ namespace Homework4.Tests.Pages.Extensions
         [TestMethod]
         public void EditControlsForTest()
         {
-            Assert.Inconclusive();
+            var obj = new htmlHelperMock<UnitView>().EditControlsFor(x => x.MeasureId);
+            Assert.IsInstanceOfType(obj, typeof(HtmlContentBuilder));
+        }
+        [TestMethod]
+        public void HtmlStringsTest()
+        {
+            var expected = new List<string> {"<div", "LabelFor", "EditorFor",
+                "ValidationMessageFor", "</div>"};
+            var actual = EditControlsForHtmlExtension.htmlString(new htmlHelperMock<MeasureView>(), x=>x.Name);
+            TestHtml.Strings(actual, expected);
         }
     }
 }
