a99d9c14658bc668580a8f3dc6b73fe91ce3262f
Tatjana Antonova <taanto@ttu.ee>
2020-03-31 13:53:32 +0300

    started p. 49

---
+++ Tests/Infra/BaseRepositoryTests.cs
@@ -0,0 +1,12 @@
+﻿using Homework4.Data.Quantity;
+using Homework4.Domain.Quantity;
+using Homework4.Infra;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Homework4.Tests.Infra
+{
+    [TestClass]
+    public class BaseRepositoryTests: AbstractClassTests<BaseRepository<Measure, MeasureData>, object>
+    {
+    }
+}
---
+++ Tests/Infra/IsInfraTested.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Homework4.Tests.Infra
+{
+    [TestClass]
+    public class IsInfraTested: AssemblyTests
+    {
+        private const string assembly = "Homework4.Infra";
+
+        protected override string Namespace(string name) { return $"{assembly}.{name}"; }
+        
+        [TestMethod]
+        public void IsQuantityTested() { isAllTested(assembly, Namespace("Quantity")); }
+
+        [TestMethod]
+        public void IsTested() { isAllTested(base.Namespace("Infra")); }
+    }
+}
