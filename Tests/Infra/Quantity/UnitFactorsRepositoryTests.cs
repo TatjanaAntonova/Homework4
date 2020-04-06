fabab7f5bebd35b3e224e017dc8d06614b55f3ec
Tatjana Antonova <taanto@ttu.ee>
2020-04-07 00:05:03 +0300

    57. osa - Infra Quantity repositooriumite testid

--- Tests/Infra/Quantity/UnitFactorsRepositoryTests.cs
+++ Tests/Infra/Quantity/UnitFactorsRepositoryTests.cs
@@ -1,12 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Homework4.Aids;
+using Homework4.Data.Quantity;
+using Homework4.Domain.Quantity;
+using Homework4.Infra;
+using Homework4.Infra.Quantity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Homework4.Tests.Infra.Quantity
 {
     [TestClass]
-    public class UnitFactorsRepositoryTests
+    public class UnitFactorsRepositoryTests : RepositoryTests<UnitFactorsRepository, UnitFactor, UnitFactorData>
     {
+        [TestInitialize]
+        public override void TestInitialize()
+        {
+            var options = new DbContextOptionsBuilder<QuantityDbContext>()
+                .UseInMemoryDatabase("TestDb")
+                .Options;
+            db = new QuantityDbContext(options);
+            dbSet = ((QuantityDbContext)db).UnitFactors;
+            obj = new UnitFactorsRepository((QuantityDbContext)db);
+            base.TestInitialize();
+        }
+
+
+        protected override Type getBaseType()
+        {
+            return typeof(PaginatedRepository<UnitFactor, UnitFactorData>);
+        }
+
+        protected override string getId(UnitFactorData d) => $"{d.SystemOfUnitsId}.{d.UnitId}";
+
+        protected override UnitFactor getObject(UnitFactorData d) => new UnitFactor(d);
+
+        protected override void setId(UnitFactorData d, string id)
+        {
+            var systemOfUnitsId = GetString.Head(id);
+            var unitId = GetString.Tail(id);
+            d.SystemOfUnitsId = systemOfUnitsId;
+            d.UnitId = unitId;
+        }
     }
 }
+
+
--- Tests/Infra/Quantity/UnitTermsRepositoryTests.cs
+++ Tests/Infra/Quantity/UnitTermsRepositoryTests.cs
@@ -1,12 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Homework4.Aids;
+using Homework4.Data.Quantity;
+using Homework4.Domain.Quantity;
+using Homework4.Infra;
+using Homework4.Infra.Quantity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Homework4.Tests.Infra.Quantity
 {
     [TestClass]
-    public class UnitTermsRepositoryTests
+    public class UnitTermsRepositoryTests : RepositoryTests<UnitTermsRepository, UnitTerm, UnitTermData>
     {
+        [TestInitialize]
+        public override void TestInitialize()
+        {
+            var options = new DbContextOptionsBuilder<QuantityDbContext>()
+                .UseInMemoryDatabase("TestDb")
+                .Options;
+            db = new QuantityDbContext(options);
+            dbSet = ((QuantityDbContext)db).UnitTerms;
+            obj = new UnitTermsRepository((QuantityDbContext)db);
+            base.TestInitialize();
+        }
+
+        protected override Type getBaseType()
+        {
+            return typeof(PaginatedRepository<UnitTerm, UnitTermData>);
+        }
+
+        protected override string getId(UnitTermData d) => $"{d.MasterId}.{d.TermId}";
+
+        protected override UnitTerm getObject(UnitTermData d) => new UnitTerm(d);
+
+        protected override void setId(UnitTermData d, string id)
+        {
+            var masterId = GetString.Head(id);
+            var termId = GetString.Tail(id);
+            d.MasterId = masterId;
+            d.TermId = termId;
+        }
     }
 }
+
